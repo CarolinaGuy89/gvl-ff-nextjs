1162f04c1a4de68add455c6d9ffbb8b2f6916ffe
Steven C <steventcruise@gmail.com>
2024-08-03 12:00:26 -0400

    basic update

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import NavbarMenu from "./components/navbar";
+
+export default function Home() {
+  return (
+
+    <main className="flex min-h-screen flex-col items-center">
+    <NavbarMenu />
+      <div>
+        Hello World
+      </div>
+    </main>
+  );
+}
