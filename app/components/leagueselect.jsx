ce3bcc19e51200b6f9cae72ed025228564e5a047
Steven C <steventcruise@gmail.com>
2024-08-04 11:37:47 -0400

    League select

---
+++ app/components/leagueselect.jsx
@@ -0,0 +1,60 @@
+'use client'
+import { useState } from "react";
+import {Button, ButtonGroup, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem} from "@nextui-org/react";
+import {ChevronDownIcon} from './ChevronDownIcon';
+
+export default function App() {
+  const [selectedOption, setSelectedOption] = useState(new Set(["gvl"]));
+
+/*   const descriptionsMap = {
+    merge:
+      "All commits from the source branch are added to the destination branch via a merge commit.",
+    squash:
+      "All commits from the source branch are added to the destination branch as a single commit.",
+    rebase: "All commits from the source branch are added to the destination branch individually.",
+  }; */
+
+  const labelsMap = {
+    gvl: "G-Vegas",
+    it: "Logistically, IT's complicated",
+    family: "League of Family Drama",
+    hockey: "Full Contact Turf Hockey",
+  }
+
+  // Convert the Set to an Array and get the first value.
+  const selectedOptionValue = Array.from(selectedOption)[0];
+
+  return (
+    <ButtonGroup variant="faded" radius="sm">
+      <Button className="text-slate-700">{labelsMap[selectedOptionValue]}</Button>
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Button isIconOnly>
+            <ChevronDownIcon />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          disallowEmptySelection
+          aria-label="Merge options"
+          selectedKeys={selectedOption}
+          selectionMode="single"
+          onSelectionChange={setSelectedOption}
+          className="max-w-[300px] text-slate-700"
+        >
+          <DropdownItem key="gvl" /*description={descriptionsMap["merge"]}*/>
+            {labelsMap["gvl"]}
+          </DropdownItem>
+          <DropdownItem key="it" /*description={descriptionsMap["squash"]}*/>
+            {labelsMap["it"]}
+          </DropdownItem>
+          <DropdownItem key="family" /*description={descriptionsMap["rebase"]}*/>
+            {labelsMap["family"]}
+          </DropdownItem>
+          <DropdownItem key="hockey" /*description={descriptionsMap["rebase"]}*/>
+            {labelsMap["hockey"]}
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </ButtonGroup>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import NavbarMenu from "./components/navbar";
+import LeagueSelect from "./components/leagueselect";
 
 export default function Home() {
   return (
 
     <main className="flex min-h-screen flex-col items-center">
-    <NavbarMenu />
+    <LeagueSelect />
       <div>
         Hello World
       </div>
