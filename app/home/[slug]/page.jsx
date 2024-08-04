ac056717cb86dec5beb0d67a589b732c18b9ff2f
Steven C <steventcruise@gmail.com>
2024-08-04 14:36:27 -0400

    Update page.jsx

--- app/home/[slug]/page.jsx
+++ app/home/[slug]/page.jsx
@@ -1,13 +1,31 @@
+'use client'
+import { useState } from "react";
 import LeagueSelect from '../../components/leagueselect'
 
+const labelsMap = {
+  gvl: "G-Vegas",
+  it: "Logistically, IT's complicated",
+  family: "League of Family Drama",
+  hockey: "Full Contact Turf Hockey",
+}
+
+const leagueValues = {
+  gvl: 1248073066,
+  it: 601844230,
+  family: 283159008,
+  hockey: 1335739020,
+}
+
 export default function Page({ params }) {
+  const [selectedOption, setSelectedOption] = useState(new Set([params.slug]));
+
+
     return (
     <main>
-      <div className="absolute top-0 right-0 h-16">
-        <LeagueSelect />
-      </div>
       <div className="flex min-h-screen flex-col items-center">
-        My Post: {params.slug}
+        My slug: {params.slug} <br />
+        My labelsmap: {labelsMap[params.slug]}<br />
+        My leagueValues:  {leagueValues[params.slug]}<br />
       </div>
     </main>
   )
