aa30509684f25c234fe127850010ab9b6ab65298
Steven C <steventcruise@gmail.com>
2024-08-09 13:29:44 -0400

    api rework

--- app/components/leagueselect.jsx
+++ app/components/leagueselect.jsx
@@ -6,13 +6,7 @@ import {ChevronDownIcon} from './ChevronDownIcon';
 export default function App() {
   const [selectedOption, setSelectedOption] = useState(new Set(["gvl"]));
 
-/*   const descriptionsMap = {
-    merge:
-      "All commits from the source branch are added to the destination branch via a merge commit.",
-    squash:
-      "All commits from the source branch are added to the destination branch as a single commit.",
-    rebase: "All commits from the source branch are added to the destination branch individually.",
-  }; */
+
 
   const labelsMap = {
     gvl: "G-Vegas",
@@ -25,7 +19,7 @@ export default function App() {
     gvl: 1248073066,
     it: 601844230,
     family: 283159008,
-    hockey: 1335739020,
+    hockey: 1335739020
   }
 
 
@@ -33,7 +27,8 @@ export default function App() {
   const selectedOptionValue = Array.from(selectedOption)[0];
 
   return (
-    <ButtonGroup variant="faded" radius="sm" classname="justify-right">
+    <body>
+    <ButtonGroup variant="faded" radius="sm" className="justify-right">
       <Button className="text-slate-700">{labelsMap[selectedOptionValue]}</Button>
       <Dropdown placement="bottom-end">
         <DropdownTrigger>
@@ -49,20 +44,21 @@ export default function App() {
           onSelectionChange={setSelectedOption}
           className="max-w-[300px] text-slate-700"
         >
-          <DropdownItem key="gvl" /*description={descriptionsMap["merge"]}*/>
+          <DropdownItem key="gvl" href="/gvl/home" /*description={descriptionsMap["merge"]}*/>
             {labelsMap["gvl"]}
           </DropdownItem>
-          <DropdownItem key="it" /*description={descriptionsMap["squash"]}*/>
+          <DropdownItem key="it" href="/it/home"/*description={descriptionsMap["squash"]}*/>
             {labelsMap["it"]}
           </DropdownItem>
-          <DropdownItem key="family" /*description={descriptionsMap["rebase"]}*/>
+          <DropdownItem key="family" href="/family/home"/*description={descriptionsMap["rebase"]}*/>
             {labelsMap["family"]}
           </DropdownItem>
-          <DropdownItem key="hockey" /*description={descriptionsMap["rebase"]}*/>
+          <DropdownItem key="hockey" href="/hockey/home" /*description={descriptionsMap["rebase"]}*/>
             {labelsMap["hockey"]}
           </DropdownItem>
         </DropdownMenu>
       </Dropdown>
     </ButtonGroup>
+    </body>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <div className="absolute top-0 right-0 h-16">
       <LeagueSelect />
-      </div>
     </html>
   );
 }
