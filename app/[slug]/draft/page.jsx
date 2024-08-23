0d3742443b7fde58222f571e27b3968b24b7ab44
Steven C. <steventcruise@gmail.com>
2024-08-23 15:50:07 -0400

    pull corrections

--- app/[slug]/draft/page.jsx
+++ app/[slug]/draft/page.jsx
@@ -28,7 +28,7 @@ export default function Home({ params }) {
       </section>
 
       <section className="chartTeamPreformance">
-      <p>This displays nothing right now.</p>
+      <p>If you see this, it is working. coming soon.</p>
       </section>
     </main>
   );
