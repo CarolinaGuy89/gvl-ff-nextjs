af3b274e0d32368f5ca316c84564d65bae31bedb
Steven C. <steventcruise@gmail.com>
2024-09-06 19:38:29 -0400

    Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import '/styles.scss'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
         <body className={inter.className} >
           {children}
+          <Analytics />
         </body>
         {/* {<LeagueSelect />} */}
     </html>
