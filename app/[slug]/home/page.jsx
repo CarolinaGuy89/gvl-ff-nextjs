3af97ca6fb0e9e6ecfb12aa9dd51bc70fe9c8dd4
Steven C. <steventcruise@gmail.com>
2025-09-12 09:28:35 -0400

    Merge infected players to league home

--- app/[slug]/home/page.jsx
+++ app/[slug]/home/page.jsx
@@ -2,38 +2,50 @@
 import React from "react";
 import BuildStandings from "./leagueStandings";
 import { useLeague } from "@/app/context/LeagueContext";
+import infectedData from "@/app/components/infectedPlayers.json";
 
-export default function LeagueOverview() {
+export default function LeagueOverview({params}) {
+  const slug = params.slug
   const { leagueStandings } = useLeague();
+  const players = infectedData.infectedPlayers
   return (
-    <main>
+    <main   className={`leagueHome ${
+    slug === "gvl" ? "with-infected" : "single-center"
+  }`}>
       <hr />
-      <section>
+      <section className="leagueStandings">
         <h2 className="chartTitle">Current League Standings</h2>
+        <div className="chartStandings">
+          <BuildStandings slug={leagueStandings} />
+        </div>
       </section>
-      <section className="chartStandings">
-        <BuildStandings slug={leagueStandings} />
+      
+      {slug === "gvl" && (
+      <section className="infectedPlayers">
+
+          <h2 className="chartTitle">Infected Players</h2>
+          <div className="infectedChart">
+          <table>
+            <thead>
+              <tr>
+                <th>Week</th>
+                <th>Infected Player</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(players).map(([week, player]) => (
+                <tr key={week}>
+                  <td>{week}</td>
+                  <td>
+                    {player || "—"} {/* show dash if empty */}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </section>
+      )}
     </main>
   );
 }
-
-// import React from "react";
-// import BuildStandings from './leagueStandings'
-
-// export default function LeagueOverview({ params }) {
-
-//   return (
-//     <main>
-//       <hr />
-
-//       <section>
-//         <h2 className="chartTitle">Current League Standings</h2>
-//       </section>
-
-//       <section className="chartStandings">
-//           <BuildStandings slug={params.slug} />
-//       </section>
-//     </main>
-//   )
-// }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -22,10 +22,7 @@ export default function MenuButton(params) {
               <Nav.Link href={`/${params.slug[0]}/matchup`}>Weekly Summary</Nav.Link>
               <Nav.Link href={`/${params.slug[0]}/team`}>Team Overview</Nav.Link>
               <Nav.Link href={`/${params.slug[0]}/draft`}>Draft Results</Nav.Link>
-              <Nav.Link href={`/${params.slug[0]}/rules`} disabled>House Rules (Coming Soon!)</Nav.Link>
-              {params.slug[0] == 'gvl' && (
-                <Nav.Link href={`/${params.slug[0]}/infected`}>Infected Players</Nav.Link>
-              )}
+              {/* <Nav.Link href={`/${params.slug[0]}/rules`} disabled>House Rules (Coming eventually!)</Nav.Link> */}
               <Nav.Link href="/">Change League</Nav.Link>
             </Nav>
           </Offcanvas.Body>
